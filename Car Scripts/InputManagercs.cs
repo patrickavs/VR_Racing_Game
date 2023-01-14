590938a2a7e58bf9eee7b21239bff235747ebd0a
Patrick Alves <114177277+patrickavs@users.noreply.github.com>
2023-01-14 13:02:39 +0100

    Add files via upload

---
+++ Car Scripts/InputManagercs.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InputCondition { 
+    KeyBoard = 0, Mobile = 1, Driving = 2
+}
+
+public class InputManager : MonoBehaviour {
+    [HideInInspector] public float gasInput;
+    [HideInInspector] public float clutchInput;
+    [HideInInspector] public float brakeInput;
+    [HideInInspector] public float steerInput;
+    [HideInInspector] public int gearInput;
+    [HideInInspector] public bool XInput;
+    [HideInInspector] public bool OInput;
+    [HideInInspector] public bool SInput;
+    [HideInInspector] public bool TInput;
+    public InputCondition inputCondition;
+
+    void Start() {
+        print("LogiSteeringInitialize: " + LogitechGSDK.LogiSteeringInitialize(false));
+    }
+
+    void Update() {
+        switch (inputCondition) {
+            case InputCondition.KeyBoard:
+                print("KEYBOARD");
+                break;
+            case InputCondition.Mobile:
+                print("MOBILE");
+                break;
+            case InputCondition.Driving:
+                print("DRIVING WHEEL");
+                if (LogitechGSDK.LogiUpdate() && LogitechGSDK.LogiIsConntected((int)LogitechKeyCode.FirstIndex)) {
+                    steerInput = LogitechInput.GetAxis("Steering Horizontal");
+                    gasInput = LogitechInput.GetAxis("Gas Vertical");
+                    brakeInput = LogitechInput.GetAxis("Brake Vertical");
+                    clutchInput = LogitechInput.GetAxis("Clutch Vertical");
+
+                    for (int i = 12; i < 19; i++) {
+                        if (LogitechInput.GetKeyTriggered(LogitechKeyCode.FirstIndex, (LogitechKeyCode)i)) {
+                            gearInput = i;
+                            print(gearInput);
+                        }
+                        if (LogitechInput.GetKeyReleased(LogitechKeyCode.FirstIndex, (LogitechKeyCode)i)) {
+                            gearInput = 0;
+                            print(gearInput);
+                        }
+                    }
+
+                    if (LogitechInput.GetKeyTriggered(LogitechKeyCode.FirstIndex, LogitechKeyCode.Cross))
+                        XINout = !XInput;
+                    if (LogitechInput.GetKeyTriggered(LogitechKeyCode.FirstIndex, LogitechKeyCode.Circle))
+                        OINout = !OInput;
+                    if (LogitechInput.GetKeyTriggered(LogitechKeyCode.FirstIndex, LogitechKeyCode.Square))
+                        SINout = !SInput;
+                    if (LogitechInput.GetKeyTriggered(LogitechKeyCode.FirstIndex, LogitechKeyCode.Triangle))
+                        TINout = !TInput;
+
+                    switch (LogitechInput.GetKeyDirectional()) {
+                        case (0); print("POV : UP")               break;
+                        case ((uint)LogitechKeyCode.UP_RIGHTButton): print("POV : UP-RIGHT\n"); break;
+                        case ((uint)LogitechKeyCode.RIGHTButton): print("POV : RIGHT\n")          break;
+                        case ((uint)LogitechKeyCode.DOWN_RIGHTButton): print("POV : DOWN-RIGHT\n")     break;
+                        case ((uint)LogitechKeyCode.DOWNButton): print("POV : DOWN\n")           break;
+                        case ((uint)LogitechKeyCode.DOWN_LEFTButton): print("POV : DOWN-LEFT\n")      break;
+                        case ((uint)LogitechKeyCode.LEFTButton): print("POV : LEFT\n")           break;
+                        case ((uint)LogitechKeyCode.UP_LEFTButton): print("POV : UP-LEFT\n")        break;
+                        default: print("CENTER\n")               break;
+                    }
+
+                    else if (!LogitechGSDK.LogiIsConnected(0)) {
+                        print("PLEASE CONNECT THE LOGITECH DRIVING FORCE WHEEL");
+                    }
+                    break;
+                }
+
+        }
+    }
+}
---
+++ Car Scripts/LogitechInput.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class LoigtechInput {
+    public static float GetAxis(string axisName) { 
+        rec = LogitechGSDK.LogiGetStateUnity(0):
+        switch (axisName) {
+            case "Steering Horizontal": return rec.lx / 32760f;
+            case "Gas Vertical": return rec.ly / -32760f;
+            case "Clutch Vertical": return rec.rglSlider[0] / -32760f;
+            case "Brake Vertical": return rec.lRz / -32760f;
+        }
+        return 0f;
+    }
+
+    public static bool GetKeyTriggered(LogitechKeyCode gamecontroller, LogitechKeyCode keyCode) { 
+        if (LogitechGSDK.LogiButtonTriggered(int)gameocntroller, (int)keyCode)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static bool getKeyPressed(LogitechKeyCode gamecontroller, LogitechKeyCode keyCode) {
+        if (LogitechGSDK.LogiButtonIsPressed((int)gamecontroller, (int)keyCode)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static bool GetKeyReleased(LogitechKeyCode gamecontroller, LogitechKeyCode keyCode) {
+        if (LoigtechGSDK.LogiButtonReleased((int)gamecontroller, (int)keyCode)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static uint GetKeyDirectional() {
+        return GetKeyDirectional().rgdwPOV[0];
+    }
+
+}
